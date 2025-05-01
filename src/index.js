892ea7dec0271d2bf04e1151a7a631ed5728fc0e
uptobe8 <ia.uptobe@gmail.com>
2025-05-01 09:52:07 +0200

    Actualizar index.js

--- src/index.js
+++ src/index.js
@@ -34,13 +34,18 @@ const convertFeature = (feature, extrusion, size) => {
     const flattened = feature.flat();
     const triangles = earcut(flattened, null, 2);
 
-    for (let i = 0; i < triangles.length; i += 3) {
-        const a = pointTo3D(flattened.slice(triangles[i] * 2, triangles[i] * 2 + 2), 0);
-        const b = pointTo3D(flattened.slice(triangles[i + 1] * 2, triangles[i + 1] * 2 + 2), 0);
-        const c = pointTo3D(flattened.slice(triangles[i + 2] * 2, triangles[i + 2] * 2 + 2), 0);
-        stl += triangleToStl(a, b, c);
+    for (let i = 0; i < scaled.features.length; i++) {
+    const geom = scaled.features[i].geometry;
+    if (geom.type === 'Polygon') {
+        const feature = geom.coordinates[0];
+        facets += convertFeature(feature, extrude, size);
+    } else if (geom.type === 'MultiPolygon') {
+        geom.coordinates.forEach(polygon => {
+            const feature = polygon[0];
+            facets += convertFeature(feature, extrude, size);
+        });
     }
-
+}
     return stl;
 };
 
